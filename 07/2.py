c03f6bdccf9f7dc10b67500c39785f41e70b4d5e
Tan Wang Leng <tanwangleng@outlook.com>
2020-12-07 20:39:24 +0800

    Add day 7 (part 2)

---
+++ 07/2.py
@@ -0,0 +1,97 @@
+import sys
+
+
+class RuleInner:
+    def __init__(self, input_inner):
+        content = input_inner.strip().split(' ')
+        self.name = ' '.join(content[1:])
+        self.amount = int(content[0])
+
+    def __repr__(self):
+        return 'RuleInner(name="{}", amount={})'.format(self.name, self.amount)
+
+
+class Rule:
+    def __init__(self, input_line):
+        line = input_line.replace('bags', '').replace('bag', '').replace('.', '')
+        main, inner = [x.strip() for x in line.split('contain')]
+        inner = [] if inner == 'no other' else [RuleInner(x.strip()) for x in inner.split(',')]
+
+        self.main = main
+        self.inner = inner
+
+    def __repr__(self):
+        return 'Rule(main="{}", inner={})'.format(self.main, self.inner)
+
+
+class GraphEdge:
+    def __init__(self, dest, value):
+        self.dest = dest
+        self.value = value
+
+    def __repr__(self):
+        return 'GraphEdge(dest="{}", value="{}")'.format(self.dest, self.value)
+
+
+class Graph:
+    def __init__(self, rules):
+        self.ad_out = {}
+        for rule in rules:
+            for rinner in rule.inner:
+                if rinner.name not in self.ad_out:
+                    self.ad_out[rinner.name] = []
+                self.ad_out[rinner.name].append(GraphEdge(rule.main, rinner.amount))
+
+            if rule.main not in self.ad_out:
+                self.ad_out[rule.main] = []
+
+        self.ad_in = {}
+        for rule in rules:
+            if rule.main not in self.ad_in:
+                self.ad_in[rule.main] = []
+
+            for rinner in rule.inner:
+                if rinner.name not in self.ad_in:
+                    self.ad_in[rinner.name] = []
+                self.ad_in[rule.main].append(GraphEdge(rinner.name, rinner.amount))
+
+    def dfs_p1(self):
+        result = set()
+
+        def dfs(name):
+            if name in result:
+                return
+            result.add(name)
+            for edge in self.ad_out[name]:
+                dfs(edge.dest)
+
+        dfs('shiny gold')
+
+        # need to discard shiny gold itself
+        return len(result) - 1
+
+    def dfs_p2(self):
+        def dfs(name):
+            count = 0
+            for edge in self.ad_in[name]:
+                count += edge.value * dfs(edge.dest)
+            return 1 + count
+
+        # need to discard shiny gold itself
+        return dfs('shiny gold') - 1
+
+    def __repr__(self):
+        return 'Graph(ad_out="{}", ad_in="{}")'.format(self.ad_out, self.ad_in)
+
+
+def main():
+    rules = [Rule(x) for x in sys.stdin]
+    graph = Graph(rules)
+
+    # print(rules)
+    # print(graph)
+
+    print(graph.dfs_p2())
+
+
+main()
