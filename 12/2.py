e63cd8138ff10de9730c7eb309ea760e760cf64e
Tan Wang Leng <tanwangleng@outlook.com>
2020-12-12 15:00:04 +0800

    Day 12: Use complex number instead

--- 12/2.py
+++ 12/2.py
@@ -1,49 +1,30 @@
 import sys
 
-def add(pos, delta, count):
-    return [pos[0] + (delta[0] * count), pos[1] + (delta[1] * count)]
-
-def rot(wp, deg):
-    if deg > 0:
-        while deg > 0:
-            deg -= 1
-            wp = [wp[1], -wp[0]]
-    elif deg < 0:
-        while deg < 0:
-            deg += 1
-            wp = [-wp[1], wp[0]]
-
-    return wp
-
-def man(pos):
-    return abs(pos[0]) + abs(pos[1])
-
 def main():
     lines = [x.strip() for x in sys.stdin]
-    pos = [0, 0]
-    wp = [10, 1]
+    pos = 0 + 0j
+    wp = 10 + 1j
 
     for l in lines:
         char = l[0]
         val = int(l[1:])
         if char == 'N':
-            wp = add(wp, [0, 1], val)
+            wp += val * 1j
         elif char == 'S':
-            wp = add(wp, [0, -1], val)
+            wp -= val * 1j
         elif char == 'E':
-            wp = add(wp, [1, 0], val)
+            wp += val
         elif char == 'W':
-            wp = add(wp, [-1, 0], val)
+            wp -= val
         elif char == 'L':
-            deg = int((-val) / 90)
-            wp = rot(wp, deg)
+            deg = int(val / 90)
+            wp *= pow(1j, deg)
         elif char == 'R':
             deg = int(val / 90)
-            wp = rot(wp, deg)
+            wp *= pow(-1j, deg)
         elif char == 'F':
-            pos = add(pos, wp, val)
+            pos += wp * val
 
-    print(str(man(pos)))
-
+    print(str(abs(int(pos.real)) + abs(int(pos.imag))))
 
 main()
