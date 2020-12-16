816499a18294439bbae0bdfdee895cd105549e6a
Tan Wang Leng <tanwangleng@outlook.com>
2020-12-16 23:10:25 +0800

    Day 13: Improve part 1

--- 13/1.py
+++ 13/1.py
@@ -1,6 +1,6 @@
 def main():
     depart = int(input())
-    buses = [int(x) for x in filter(lambda x: x != 'x', [x for x in input().split(',')])]
+    buses = [int(x) for x in input().split(',') if x != 'x']
     next_time = [[x, x - (depart % x) if (depart % x) != 0 else 0] for x in buses]
     best = min(next_time, key=lambda x: x[1])
     print(str(best[0] * best[1]))
