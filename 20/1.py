3503f7e13528f2b7c070a1384a4db865e0142b82
Tan Wang Leng <tanwangleng@outlook.com>
2020-12-24 14:14:52 +0800

    Day 20: Improve part 1's code

--- 20/1.py
+++ 20/1.py
@@ -1,5 +1,9 @@
+W, H = 10, 10
+
+
+# get a unique edge ID for a particular edge
 def gen_edge_uid(pix):
-    # left to right
+    # go from left to right
     ltr = 0
     cur = 1
     for i in range(len(pix) - 1, -1, -1):
@@ -7,7 +11,7 @@ def gen_edge_uid(pix):
             ltr += cur
         cur *= 2
 
-    # right to left
+    # go from right to left
     rtl = 0
     cur = 1
     for i in range(len(pix)):
@@ -15,28 +19,32 @@ def gen_edge_uid(pix):
             rtl += cur
         cur *= 2
 
+    # always get the smaller of the two
+    # which guarantees uniqueness
     return min(ltr, rtl)
 
 
-def main():
-    W, H = 10, 10
+class Tile:
+    def __init__(self, tid, pixels):
+        self.tid = tid
+        self.pixels = pixels
 
+        top_id = gen_edge_uid(list(pixels[0]))
+        bot_id = gen_edge_uid(list(pixels[H - 1]))
+        left_id = gen_edge_uid([pixels[i][0] for i in range(len(pixels))])
+        right_id = gen_edge_uid([pixels[i][W - 1] for i in range(len(pixels))])
+        self.edge_uids = [top_id, bot_id, left_id, right_id]
+
+
+def main():
     tiles = {}
 
     try:
         while True:
             header = input()
             tid = int(header.split(' ')[1][:-1])
-
-            pixels = []
-            while len(pixels) != H:
-                pixels.append(input())
-
-            top_id = gen_edge_uid(list(pixels[0]))
-            bot_id = gen_edge_uid(list(pixels[H - 1]))
-            left_id = gen_edge_uid([pixels[i][0] for i in range(len(pixels))])
-            right_id = gen_edge_uid([pixels[i][W - 1] for i in range(len(pixels))])
-            tiles[tid] = [top_id, bot_id, left_id, right_id]
+            pixels = [input() for x in range(H)]
+            tiles[tid] = Tile(tid, pixels)
 
             # discard blank line
             input()
@@ -44,10 +52,9 @@ def main():
     except EOFError:
         pass
 
-
     common_edges = {}
     for tid in tiles:
-        for edge in tiles[tid]:
+        for edge in tiles[tid].edge_uids:
             if edge not in common_edges:
                 common_edges[edge] = []
             common_edges[edge].append(tid)
@@ -55,7 +62,7 @@ def main():
     corner_tids = 1
     for tid in tiles:
         isolated = 0
-        for edge in tiles[tid]:
+        for edge in tiles[tid].edge_uids:
             if len(common_edges[edge]) == 1:
                 isolated += 1
         if isolated == 2:
